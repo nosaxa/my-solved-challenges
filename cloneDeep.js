7f6972157b8685e7f674d891277164b4b8513a84
Maxim <nosaxa@protonmail.com>
2021-12-14 21:41:45 +0100

    Create my own clone deep function

---
+++ cloneDeep.js
@@ -0,0 +1,36 @@
+/* #hexlet #objects #recursion #lodash
+
+Реализуйте и экспортируйте по умолчанию функцию, которая выполняет глубокое копирование объектов.
+Для реализации этой задачи нельзя использовать функцию cloneDeep() библиотеки lodash.
+
+result имеет такую же структуру, как и data
+const result = cloneDeep(data);
+
+Но внутри другие объекты
+result.key2 !== data.key2; // true
+result.key2.innerKey !== data.key2.innerKey; // true 
+
+*/
+
+import isObject from "lodash/isObject";
+
+const data = {
+  key: "value",
+  key2: {
+    key: "innerValue",
+    innerKey: {
+      anotherKey: "anotherValue",
+    },
+  },
+};
+
+// 1.
+const cloneDeep = (object) => {
+  const result = {};
+  const entries = Object.entries(object);
+  for (const [key, value] of entries) {
+    result[key] = isObject(value) ? cloneDeep(value) : value;
+  }
+
+  return result;
+};
